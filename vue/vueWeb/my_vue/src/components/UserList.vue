541b92ef28ab77c2ca47acc328881f82acda9d63
yankai <yanakai@126.com>
2019-12-14 17:37:59 +0800

    删除没有用的页面

---
+++ vue/vueWeb/my_vue/src/components/UserList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <el-table :data="userData" border justify="center" style="width:100%">
+            <el-table-column prop="userName" label="登录用户"></el-table-column>
+            <el-table-column prop="trueName" label="真实姓名"></el-table-column>
+            <el-table-column prop="email" label="邮箱"></el-table-column>
+            <el-table-column prop="phoneNumber" label="手机号"></el-table-column>
+            <el-table-column prop="sex" label="性别"></el-table-column>
+        </el-table>
+    </div>
+</template>
+<script>
+import Vue from "vue"
+export default {
+    name: "userDataList",
+    data(){
+        return {
+            userData:[]
+        }
+    },
+    //在这里页面初始化时调用ajax请求方法
+    created(){
+      this.initUser();
+    },
+    methods : {
+        initUser: function(){
+            var url = "/api/user/list/";
+            Vue.axios.get(url).then((response)=>{
+                this.userData = response.data.rows;
+                console.log("response: "+response.data.rows);
+            }).catch((error)=>{
+                console.log("error!"+error);
+            });
+        }
+    }
+}
+</script>
--- vue/vueWeb/my_vue/src/router/index.js
+++ vue/vueWeb/my_vue/src/router/index.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import CopClass from '@/components/EnterPage'
+import userList from '@/components/UserList'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
       path: '/enter/',
       name: 'CopName',
       component: CopClass
+    },
+    {
+      path: "/user/list/",
+      name: "userDataList",
+      component: userList
     }
   ]
 })
