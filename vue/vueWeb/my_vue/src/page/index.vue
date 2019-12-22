ab1a209ac4543fd623ecbf05db3bc0858bdebd79
yankai <yanakai@126.com>
2019-12-22 14:19:47 +0800

    删除不需要的图片

--- vue/vueWeb/my_vue/src/page/index.vue
+++ vue/vueWeb/my_vue/src/page/index.vue
@@ -1,55 +1,53 @@
 <template>
-    <div>
-       <el-container>
-           <el-header >
-                <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-                    <el-radio-button :label="false">展开</el-radio-button>
-                    <el-radio-button :label="true">收起</el-radio-button>
-                </el-radio-group>
-           </el-header>
-            <el-container>
-                <el-aside width="200px" >
-                    <el-menu
-                        default-active="$route.path"
-                        @open="handleOpen"  
-                        @close="handleClose"
-                        class="el-menu-vertical-demo"
-                        router  unique-opened	
-                        :collapse="isCollapse"
-                        :default-openeds="open_list"
-                        background-color="#545c64"
-                        text-color="#fff"
-                        active-text-color="#ffd04b">
-                        <el-submenu index="1">
-                            <template slot="title">
-                                <i class="el-icon-document"></i>
-                                <span>文章管理</span>
-                            </template>
-                            <el-menu-item  index="/article/list">文章管理</el-menu-item>
-                            <el-menu-item index="/column/list">栏目管理</el-menu-item>
+    <el-container>
+        <el-header >
+            <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+                <el-radio-button :label="false">展开</el-radio-button>
+                <el-radio-button :label="true">收起</el-radio-button>
+            </el-radio-group>
+        </el-header>
+        <el-container>
+            <el-aside width="200px" >
+                <el-menu
+                    default-active="$route.path"
+                    @open="handleOpen"  
+                    @close="handleClose"
+                    class="el-menu-vertical-demo"
+                    router  unique-opened	
+                    :collapse="isCollapse"
+                    :default-openeds="open_list"
+                    background-color="#545c64"
+                    text-color="#fff"
+                    active-text-color="#ffd04b">
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-document"></i>
+                            <span>文章管理</span>
+                        </template>
+                        <el-menu-item  index="/article/list">文章管理</el-menu-item>
+                        <el-menu-item index="/column/list">栏目管理</el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="2">
+                        <template slot="title">
+                            <i class="el-icon-menu"></i>
+                            <span>系统管理</span>
+                        </template>
+                        <el-menu-item  index="/user/list">用户管理</el-menu-item>
+                        <el-menu-item index="/role/list">角色管理</el-menu-item>
+                        <el-menu-item index="/menu/list">菜单管理</el-menu-item>
+                        <el-submenu index="#">
+                            <template slot="title">选项4</template>
+                            <el-menu-item index="#">选项1</el-menu-item>
                         </el-submenu>
-                        <el-submenu index="2">
-                            <template slot="title">
-                                <i class="el-icon-menu"></i>
-                                <span>系统管理</span>
-                            </template>
-                            <el-menu-item  index="/user/list">用户管理</el-menu-item>
-                            <el-menu-item index="/role/list">角色管理</el-menu-item>
-                            <el-menu-item index="/menu/list">菜单管理</el-menu-item>
-                            <el-submenu index="#">
-                                <template slot="title">选项4</template>
-                                <el-menu-item index="#">选项1</el-menu-item>
-                            </el-submenu>
-                        </el-submenu>
-                        
-                    </el-menu>
-                </el-aside>
-                <el-main>
-                    <router-view/>
-                </el-main>
-            </el-container>
+                    </el-submenu>
+                    
+                </el-menu>
+            </el-aside>
+            <el-main>
+                <router-view/>
+            </el-main>
         </el-container>
-    </div>
+    </el-container>
 </template>
 <style>
     .el-menu-vertical-demo:not(.el-menu--collapse) {
