a07c8f48a9e9026a448b13e038d71b24ec6d87a7
yankai <yanakai@126.com>
2019-12-23 17:52:51 +0800

    增加左侧头部内容区

--- vue/vueWeb/vue_blog/src/mock/modules/login.js
+++ vue/vueWeb/vue_blog/src/mock/modules/login.js
@@ -3,18 +3,19 @@
  */
 
 // 登录接口
+// 登录接口
 export function login () {
-    return {
-      // isOpen: false,
-      url: 'http://localhost:8090/login',
-      type: 'get',
-      data: {
-        'msg': 'success',
-        'code': 0,
-        'data': {
-          'token': '4344323121398'
-          // 其他数据
-        }
+  return {
+    // isOpen: false,
+    url: 'http://localhost:8090/login',
+    type: 'post',
+    data: {
+      'msg': 'success',
+      'code': 0,
+      'data': {
+        'token': '4344323121398'
+        // 其他数据
       }
     }
   }
+}
--- vue/vueWeb/vue_blog/src/router/index.js
+++ vue/vueWeb/vue_blog/src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Cookies from "js-cookie"
 import Login from '@/views/Login'
 import Index from '@/views/Index'
 import NotFound from '@/views/404'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -25,3 +26,28 @@ export default new Router({
     }
   ]
 })
+
+//添加导航守卫，如下图所示，根据用户登录会话记录，路由到主页或登录界面。
+router.beforeEach((to, from, next) => {
+  // 登录界面登录成功之后，会把用户信息保存在会话
+  // 存在时间为会话生命周期，页面关闭即失效。
+  let token = Cookies.get('token')
+  let user = sessionStorage.getItem('user')
+  if (to.path === '/login') {
+    // 如果是访问登录界面，如果用户会话信息存在，代表已登录过，跳转到主页
+    if(token) {
+      next({ path: '/' })
+    } else {
+      next()
+    }
+  } else {
+    if (!token) {
+      // 如果访问非登录界面，且户会话信息不存在，代表未登录，则跳转到登录界面
+      next({ path: '/login' })
+    } else {
+      next()
+    }
+  }
+})
+
+export default router
