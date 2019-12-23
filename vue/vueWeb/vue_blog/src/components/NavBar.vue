3223085f4c5063e2b8f08ddad2cb0f1c81cadbb7
yankai <yanakai@126.com>
2019-12-23 23:25:19 +0800

    修改左侧导航

--- vue/vueWeb/vue_blog/src/components/NavBar.vue
+++ vue/vueWeb/vue_blog/src/components/NavBar.vue
@@ -1,81 +1,103 @@
 <template>
-    <div class="menu-bar-container">
+	<div class="menu-bar-container">
     <!-- logo -->
-    <div class="logo" :class="isCollapse?'menu-bar-collapse-width':'menu-bar-width'">
-        <img :src="this.logo" /> <div>{{isCollapse?'':sysName}}</div>
+    <div class="logo" :style="{'background-color':themeColor}" :class="collapse?'menu-bar-collapse-width':'menu-bar-width'"
+      @click="$router.push('/')">
+        <img v-if="collapse" src="@/assets/logo.png"/> <div>{{collapse?'':appName}}</div>
     </div>
     <!-- 导航菜单 -->
-    <el-menu default-active="1-1" :class="isCollapse?'menu-bar-collapse-width':'menu-bar-width'" @open="handleopen" @close="handleclose" @select="handleselect" :collapse="isCollapse">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">{{$t("sys.sysMng")}}</span>
-        </template>
-        <el-menu-item index="1-1" @click="$router.push('user')">{{$t("sys.userMng")}}</el-menu-item>
-        <el-menu-item index="1-2" @click="$router.push('dept')">{{$t("sys.deptMng")}}</el-menu-item>
-        <el-menu-item index="1-3" @click="$router.push('role')">{{$t("sys.roleMng")}}</el-menu-item>
-        <el-menu-item index="1-4" @click="$router.push('menu')">{{$t("sys.menuMng")}}</el-menu-item>
-        <el-menu-item index="1-5" @click="$router.push('log')">{{$t("sys.logMng")}}</el-menu-item>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">{{$t("sys.sysMonitor")}}</span>
-        </template>
-      </el-submenu>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">{{$t("sys.nav3")}}</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">{{$t("sys.nv4")}}</span>
-      </el-menu-item>
+    <el-menu ref="navmenu" default-active="1" :class="collapse?'menu-bar-collapse-width':'menu-bar-width'"
+      :collapse="collapse" :collapse-transition="false" :unique-opened="true  "
+      @open="handleopen" @close="handleclose" @select="handleselect">
+      <!-- 导航菜单树组件，动态加载菜单 -->
+      <menu-tree v-for="item in navTree" :key="item.id" :menu="item"></menu-tree>
     </el-menu>
-    </div>
+	</div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import MenuTree from "@/components/MenuTree"
 export default {
-  data() {
-    return {
-      isCollapse: false,
-      sysName: "",
-      logo: "",
-    };
+  components:{
+        MenuTree
+  },
+  computed: {
+    ...mapState({
+      appName: state=>state.app.appName,
+      themeColor: state=>state.app.themeColor,
+      collapse: state=>state.app.collapse,
+      navTree: state=>state.menu.navTree
+    }),
+    mainTabs: {
+      get () { return this.$store.state.tab.mainTabs },
+      set (val) { this.$store.commit('updateMainTabs', val) }
+    },
+    mainTabsActiveName: {
+      get () { return this.$store.state.tab.mainTabsActiveName },
+      set (val) { this.$store.commit('updateMainTabsActiveName', val) }
+    }
+  },
+  watch: {
+    $route: 'handleRoute'
+  },
+  created () {
+    this.handleRoute(this.$route)
   },
   methods: {
     handleopen() {
-      console.log('handleopen');
+      console.log('handleopen')
     },
     handleclose() {
-      console.log('handleclose');
+      console.log('handleclose')
     },
     handleselect(a, b) {
-      console.log('handleselect');
+      console.log('handleselect')
+    },
+    // 路由操作处理
+    handleRoute (route) {
+      // tab标签页选中, 如果不存在则先添加
+      var tab = this.mainTabs.filter(item => item.name === route.name)[0]
+      if (!tab) {
+        tab = {
+          name: route.name,
+          title: route.name,
+          icon: route.meta.icon
+        }
+        this.mainTabs = this.mainTabs.concat(tab)
+      }
+      this.mainTabsActiveName = tab.name
+      // 切换标签页时同步更新高亮菜单
+      if(this.$refs.navmenu != null) {
+        this.$refs.navmenu.activeIndex = '' + route.meta.index
+        this.$refs.navmenu.initOpenedMenu()
+      }
     }
-  },
-  mounted() {
-    this.sysName = "I like Kitty";
-    this.logo = require("@/assets/logo.png");
   }
-};
+}
 </script>
 
 <style scoped lang="scss">
 .menu-bar-container {
+  position: fixed;
+  top: 0px;
+  left: 0;
+  bottom: 0;
+  z-index: 1020;
   .el-menu {
     position:absolute;
     top: 60px;
     bottom: 0px;
     text-align: left;
+    // background-color: #2968a30c;
   }
   .logo {
     position:absolute;
     top: 0px;
     height: 60px;   
     line-height: 60px;
-    background: #4b5f6e;
+    background: #545c64;
+    cursor:pointer;
     img {
         width: 40px;
         height: 40px;
@@ -84,9 +106,10 @@ export default {
         float: left;
     }
     div {
-      font-size: 22px;
+      font-size: 25px;
       color: white;
       text-align: left;
+      padding-left: 20px;
     }
   }
   .menu-bar-width {
@@ -96,4 +119,5 @@ export default {
     width: 65px;
   }
 }
+
 </style>
--- vue/vueWeb/vue_blog/src/main.js
+++ vue/vueWeb/vue_blog/src/main.js
@@ -8,13 +8,14 @@ import 'element-ui/lib/theme-chalk/index.css' //引入element-ui模块
 import api from './http'
 import i18n from './i18n' //加入i18n
 import store from './store'
+import global from '@/utils/global'
 import 'font-awesome/css/font-awesome.min.css'
 import '@assets/iconfont/iconfont.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.use(api); //以 vue 插件的形式引入 axios，这样在其他地方就可通过 this.$api 调用相关的接口了
-
+Vue.prototype.global = global
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
