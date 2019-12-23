1953d65ece7032373e2fd9672748e39056ea3a98
yankai <yanakai@126.com>
2019-12-23 11:13:33 +0800

    新增对axios请求封装，增加axios复用性

--- vue/vueWeb/vue_blog/src/main.js
+++ vue/vueWeb/vue_blog/src/main.js
@@ -5,8 +5,11 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui' //引入element-ui模块
 import 'element-ui/lib/theme-chalk/index.css' //引入element-ui模块
+import api from './http/index'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(api); //以 vue 插件的形式引入 axios，这样在其他地方就可通过 this.$api 调用相关的接口了
 
 /* eslint-disable no-new */
 new Vue({
