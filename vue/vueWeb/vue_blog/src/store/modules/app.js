976982a914d4188ed46d99ce89c0b55830190bd7
yankai <yanakai@126.com>
2019-12-25 22:20:35 +0800

    修改主题默认样式

--- vue/vueWeb/vue_blog/src/store/modules/app.js
+++ vue/vueWeb/vue_blog/src/store/modules/app.js
@@ -1,7 +1,7 @@
 export default {
     state: {
         appName: "VBlog",  // 应用名称
-        themeColor: "#14889A",  // 主题颜色
+        themeColor: "#414748",  // 主题颜色
         oldThemeColor: "#14889A",   // 上一次主题颜色
         collapse:false,  // 导航栏收缩状态
         menuRouteLoaded:false    // 菜单和路由是否已经加载
