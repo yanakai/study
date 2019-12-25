269e25194f45350ff3e2a33f6191547588f9d4c0
yankai <yanakai@126.com>
2019-12-25 22:06:47 +0800

    修复问题

--- vue/vueWeb/vue_blog/src/components/NavBar.vue
+++ vue/vueWeb/vue_blog/src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <!-- logo -->
     <div class="logo" :style="{'background-color':themeColor}" :class="collapse?'menu-bar-collapse-width':'menu-bar-width'"
       @click="$router.push('/')">
-        <img v-if="collapse" src="@/assets/logo.png"/> <div>{{collapse?'':appName}}</div>
+        <img v-if="collapse" src="@/assets/user.png"/> <div>{{collapse?'':appName}}</div>
     </div>
     <!-- 导航菜单 -->
     <el-menu ref="navmenu" default-active="1" :class="collapse?'menu-bar-collapse-width':'menu-bar-width'"
