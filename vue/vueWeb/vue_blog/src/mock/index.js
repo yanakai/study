f4ead71c2714ef651c058ed6b8eca7858e496501
yankai <yanakai@126.com>
2019-12-25 11:26:48 +0800

    修改mock拦截不走后台请求

--- vue/vueWeb/vue_blog/src/mock/index.js
+++ vue/vueWeb/vue_blog/src/mock/index.js
@@ -11,8 +11,8 @@ import * as log from './modules/log'
 // 1. 开启/关闭[所有模块]拦截, 通过调[openMock参数]设置.
 // 2. 开启/关闭[业务模块]拦截, 通过调用fnCreate方法[isOpen参数]设置.
 // 3. 开启/关闭[业务模块中某个请求]拦截, 通过函数返回对象中的[isOpen属性]设置.
-// let openMock = true
-let openMock = false
+let openMock = true //不走后台请求
+//let openMock = false  //走后台请求接口
 fnCreate(login, openMock)
 fnCreate(user, openMock)
 fnCreate(role, openMock)
--- vue/vueWeb/vue_blog/src/utils/global.js
+++ vue/vueWeb/vue_blog/src/utils/global.js
@@ -5,7 +5,7 @@
  */
 
 // 后台管理系统服务器地址
-export const baseUrl = 'http://localhost:8001'
+export const baseUrl = 'http://localhost:8080'
 // 系统数据备份还原服务器地址
 export const backupBaseUrl = 'http://localhost:8002'
 
