7586d011490c3a619e59c9cf97a4c712cf288e6f
yankai <yanakai@126.com>
2020-01-01 02:55:54 +0800

    增加左上角图标

--- vue/vueWeb/my_vue/src/components/HeadBar.vue
+++ vue/vueWeb/my_vue/src/components/HeadBar.vue
@@ -6,6 +6,22 @@
                 <el-menu-item index="1" @click="onCollapse"><hamburger :isActive="collapse"></hamburger></el-menu-item>
             </el-menu>
         </span>
+        <span class="userinfo">
+             <el-menu class="el-menu-demo" :background-color="themeColor" :text-color="themeColor" :active-text-color="themeColor" mode="horizontal">
+                <el-submenu index="1"  popper-class="infoItem">
+                    <template slot="title">
+                        <div class='welcome'>
+                            <span class="name">你好,</span>
+                            <span class='name avatarname'>闫凯</span>
+                        </div>
+                        <img src="@/assets/img/avatar-2.jpg" class='avatar' alt="">
+                    </template>
+                    <el-menu-item index="1-1" @click="setDialogInfo('info')" style="color:white">个人信息</el-menu-item>
+                    <el-menu-item index="1-2" @click="setDialogInfo('pass')" style="color:white">修改信息</el-menu-item>
+                    <el-menu-item index="1-3" @click="setDialogInfo('logout')" style="color:white">退出</el-menu-item>
+                </el-submenu>
+             </el-menu>
+        </span>
     </div>
 </template>
 
@@ -34,7 +50,7 @@ export default{
     }
  }
 </script>
-<style>
+<style lang="scss">
 .headbar {
   position: fixed;
   top: 0;
@@ -54,5 +70,61 @@ export default{
 }
 .position-collapse-left {
   left: 65px;
+}
+.userinfo {
+    line-height: 60px;
+    text-align:right;
+}
+.el-menu-demo .el-submenu{
+    float: right;
+}
+.avatar{
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    vertical-align: middle;
+    display: inline-block;
+}
+.langAvatar{
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    vertical-align: middle;
+    display: inline-block;
+}
+.welcome{
+    display: inline-block;
+    vertical-align: middle;
+    padding: 0 5px;
+    .name{
+        line-height: 20px;
+        text-align: center;
+        font-size: 12px;
+        color: white;
+    }
+    .avatarname{
+        color:#a9d86e;
+        font-weight:bolder;
+        font-size: 13px;
+    }
+}
+.username {
+    cursor: pointer;
+    margin-right: 5px;
+}
+.border{
+    border:1px solid;
+}
+.notify-row{
+    line-height:60px;
+    flex:1;
+    ul{
+        display: flex;
+        justify-content: space-around;
+    }
+}
+
+ul.top-menu > li {
+    position: relative;
 }
 </style>
