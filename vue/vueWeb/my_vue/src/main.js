f437851bc38654a84cdfd401b7fbc9ffc23aca88
yankai <yanakai@126.com>
2020-01-09 11:36:34 +0800

    增加http的api公共请求

--- vue/vueWeb/my_vue/src/main.js
+++ vue/vueWeb/my_vue/src/main.js
@@ -6,16 +6,14 @@ import router from './router'
 import ElementUI from 'element-ui'  //引入element-ui模块
 import 'element-ui/lib/theme-chalk/index.css'  //引入element-ui模块
 import 'font-awesome/css/font-awesome.min.css'
-import axios from 'axios'   //引入axios 模块
-import VueAxios from 'vue-axios'  // 引入axios 模块
+import api from './http'
 import store from './store' //存放全局的参数值
 import i18n from './i18n' //加入i18n
 import global from '@/utils/global' //加入全局变量
 
 Vue.config.productionTip = false
 Vue.use(ElementUI) //引入element-ui模块
-Vue.use(VueAxios,axios) //引入axios 模块、
-
+Vue.use(api); //以 vue 插件的形式引入 axios，这样在其他地方就可通过 this.$api 调用相关的接口了
 Vue.prototype.global = global
 
 // 创建全局过滤器
